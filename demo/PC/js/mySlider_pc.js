d42b1b81214160782f55d728046f995b4e9079e6
Ming Xu <gmbody7@gmail.com>
2016-04-17 02:07:00 -0700

    fix pid in pc share

--- demo/PC/js/mySlider_pc.js
+++ demo/PC/js/mySlider_pc.js
@@ -2,7 +2,24 @@ $(function(){
 	var page_wrap = $('#page_wrap');
 	var viewScale = page_wrap.width()/page_wrap.height();
 	var def = $.Deferred();
+    
+    var getUrlParameter = function getUrlParameter(sParam) {
+		var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+			sURLVariables = sPageURL.split('&'),
+			sParameterName,
+			i;
 
+		for (i = 0; i < sURLVariables.length; i++) {
+			sParameterName = sURLVariables[i].split('=');
+
+			if (sParameterName[0] === sParam) {
+				return sParameterName[1] === undefined ? true : sParameterName[1];
+			}
+		}
+	};
+
+	var pid = getUrlParameter('pid');
+	
 	//h5首页数据接口
 	$.ajax({
 		 	url : 'http://www.boluomi1314.com:8083/info/data/view',
