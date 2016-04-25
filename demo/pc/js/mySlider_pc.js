57d77fe54955185da018e4b24f60910cd142046d
Ming Xu <gmbody7@gmail.com>
2016-04-24 21:37:46 -0700

    fix

--- demo/pc/js/mySlider_pc.js
+++ demo/pc/js/mySlider_pc.js
@@ -194,7 +194,7 @@ $(function(){
 
 
 		//图片预加载
-		var imgList = [serImg_data[0].pic,'albumBg.png','light.png','btn_arrow.png','music-icon.png','loading.gif','cotyRight.png','indexIcon.png','musicIcon.png','playingBg.png','workingIcon.png','cover.png'];
+		var imgList = [serImg_data[0].pic,'img/albumBg.png','img/light.png','img/btn_arrow.png','img/music-icon.png','img/loading.gif','img/copyRight.png','img/indexIcon.png','img/musicIcon.png','img/playingBg.png','img/workingIcon.png','img/cover.png'];
 
 		loadImg(imgList);
 		function loadImg(list){
